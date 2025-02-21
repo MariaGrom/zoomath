89ed80fd000dc6912595654bafb1c1c132d47aa3
Maria Gromyko <mazvereva91@mail.ru>
2025-02-21 16:55:00 +0300

    add link for create card in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import styles from './Header.module.scss';
-import { Link } from 'react-router-dom';
-import IconUser from '../../img/user-login.svg'
+import React from "react";
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import IconUser from "../../img/user-login.svg";
 
 function Header() {
-    return (
-        <section className={styles.header}>
-            <div className={styles.container}>
-                <div className={styles.logo}>LOGO</div>
-                <nav>
-                    <ul className={styles.list}>
-                        <li><a href='#'>Подписка</a></li>
-                        <li><a href='#'>О лицеях</a></li>
-                        <li><a href='#'>Отзывы</a></li>
-                    </ul>
-                </nav>
-                <Link to='/signin' className={styles.btn_login}><img src={IconUser} alt='userLogin' className={styles.icon_user} />Войти</Link>
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo}>LOGO</div>
+        <nav>
+          <ul className={styles.list}>
+            <li>
+              <a href="#">Подписка</a>
+            </li>
+            <li>
+              <a href="#">О лицеях</a>
+            </li>
+            <li>
+              <a href="#">Отзывы</a>
+            </li>
+            <li>
+              <Link to="/createcard">Создать карточку</Link>
+            </li>
+          </ul>
+        </nav>
+        <Link to="/signin" className={styles.btn_login}>
+          <img src={IconUser} alt="userLogin" className={styles.icon_user} />
+          Войти
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default Header;
